d817b74e86192cc474e6c768cbda27a2f97d386b
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-11 04:07:09 +0200

    Додано ViewModelFactory для створення фабрики з різним числом конструкторів для різних вьюмоделей

---
+++ core/src/main/java/com/study/core/views/ViewModelFactory.kt
@@ -0,0 +1,43 @@
+package com.study.core.views
+
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import java.lang.reflect.Constructor
+
+class ViewModelFactory(
+    private val dependencies: List<Any>,
+    owner: SavedStateRegistryOwner
+) : AbstractSavedStateViewModelFactory(owner, null) {
+
+    override fun <T : ViewModel> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
+        val constructors = modelClass.constructors
+        val constructor = constructors.maxByOrNull { it.typeParameters.size }!!
+
+        // - SavedStateHandle is also a dependency from screen VM scope, but we can obtain it only here,
+        //   that's why merging it with the list of other dependencies:
+        val dependenciesWithSavedState = dependencies + handle
+
+        // generating the list of arguments to be passed into the view-model's constructor
+        val arguments = findDependencies(constructor, dependenciesWithSavedState)
+
+        // creating view-model
+        return constructor.newInstance(*arguments.toTypedArray()) as T
+    }
+
+    private fun findDependencies(constructor: Constructor<*>, dependencies: List<Any>): List<Any> {
+        val args = mutableListOf<Any>()
+        // here we iterate through view-model's constructor arguments and for each
+        // argument we search dependency that can be assigned to the argument
+        constructor.parameterTypes.forEach { parameterClass ->
+            val dependency = dependencies.first { parameterClass.isAssignableFrom(it.javaClass) }
+            args.add(dependency)
+        }
+        return args
+    }
+}
