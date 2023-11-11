92c4dd5c8dec60af6231087010b9d063777a91d1
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-11 04:05:17 +0200

    Додано BaseViewModel конструктор для усіх ViewModels

---
+++ core/src/main/java/com/study/core/views/BaseViewModel.kt
@@ -0,0 +1,24 @@
+package com.study.core.views
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+import androidx.lifecycle.ViewModel
+import com.study.core.utils.Event
+
+typealias LiveEvent<T> = LiveData<Event<T>>
+typealias MutableLiveEvent<T> = MutableLiveData<Event<T>>
+
+/**
+ * Базовий клас для усіх [ViewModel]
+ */
+class BaseViewModel: ViewModel() {
+
+    /**
+     * Додай це метод у дочірній клас, якщо хочешь
+     * слухати результат з інших єкранів
+     */
+    open fun onResult(result: Any) {
+
+    }
+}
