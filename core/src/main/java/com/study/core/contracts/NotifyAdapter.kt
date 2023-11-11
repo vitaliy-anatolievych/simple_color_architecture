31212a91cb905719ee69180d0366b230831d3b25
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-11 07:26:56 +0200

    Fix: змінена технологія передачі containerId

--- core/src/main/java/com/study/core/contracts/NotifyAdapter.kt
+++ core/src/main/java/com/study/core/contracts/NotifyAdapter.kt
@@ -1,6 +1,11 @@
 package com.study.core.contracts
 
+import org.intellij.lang.annotations.Identifier
+
 interface NotifyAdapter {
+
+    @Identifier
+    val containerId: Int
 
     fun notifyScreenUpdates()
 }
