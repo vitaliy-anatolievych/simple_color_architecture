cb5754cce3fc9da099647fe668ab0d3bd9f8971f
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-13 00:30:54 +0200

    Тепер потрібно передавати анімації в конструкторі классом

---
+++ core/src/main/java/com/study/core/utils/Animations.kt
@@ -0,0 +1,10 @@
+package com.study.core.utils
+
+import androidx.annotation.AnimRes
+
+class Animations (
+    @AnimRes val enterAnim: Int,
+    @AnimRes val exitAnim: Int,
+    @AnimRes val popEnterAnim: Int,
+    @AnimRes val popExitAnim: Int,
+)
