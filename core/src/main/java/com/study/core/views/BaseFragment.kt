6236f0fe4deee5e8ec27b55bf523c8ee03e7fdd6
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-23 20:38:08 +0200

    Додано до BaseFragment renderResult

--- core/src/main/java/com/study/core/views/BaseFragment.kt
+++ core/src/main/java/com/study/core/views/BaseFragment.kt
@@ -1,7 +1,14 @@
 package com.study.core.views
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import com.study.core.contracts.FragmentsHolder
+import com.study.core.utils.ErrorResult
+import com.study.core.utils.PendingResult
+import com.study.core.utils.Result
+import com.study.core.utils.SuccessResult
 
 /**
  * Базовий клас для усіх фрагментів
@@ -19,5 +26,20 @@ abstract class BaseFragment: Fragment() {
      */
     fun notifyScreenUpdates() {
         (requireActivity() as FragmentsHolder).notifyScreenUpdates()
+    }
+
+    fun <T> renderResult(
+        root: View,
+        result: Result<T>,
+        onPending: () -> Unit,
+        onError: (Exception) -> Unit,
+        onSuccess: (T) -> Unit
+    ) {
+        (root as ViewGroup).children.forEach { it.visibility = View.GONE }
+        when(result) {
+            is ErrorResult -> onError(result.exception)
+            is PendingResult -> onPending()
+            is SuccessResult -> onSuccess(result.data)
+        }
     }
 }
