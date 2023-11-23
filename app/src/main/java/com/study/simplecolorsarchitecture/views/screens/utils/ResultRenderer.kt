b35d27dac5928efa443d5c6547d0ccbd732f808d
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-23 23:34:41 +0200

    Додано ResultRenderer для спрощення управління зміни стану на єкрані

---
+++ app/src/main/java/com/study/simplecolorsarchitecture/views/screens/utils/ResultRenderer.kt
@@ -0,0 +1,44 @@
+package com.study.simplecolorsarchitecture.views.screens.utils
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.view.children
+import com.study.core.views.BaseFragment
+import com.study.simplecolorsarchitecture.R
+import com.study.simplecolorsarchitecture.databinding.PartResultBinding
+import com.study.core.utils.Result
+
+/**
+ * Default [Result] rendering.
+ * - if [result] is [PendingResult] -> only progress-bar is displayed
+ * - if [result] is [ErrorResult] -> only error container is displayed
+ * - if [result] is [SuccessResult] -> error container & progress-bar is hidden, all other views are visible
+ */
+fun <T> BaseFragment.renderSimpleResult(root: ViewGroup, result: Result<T>, onSuccess: (T) -> Unit) {
+    val binding = PartResultBinding.bind(root)
+
+    renderResult(
+        root = root,
+        result = result,
+        onPending = {
+            binding.progressBar.visibility = View.VISIBLE
+        },
+        onError = {
+            binding.errorContainer.visibility = View.VISIBLE
+        },
+        onSuccess = { successData ->
+            root.children
+                .filter { it.id != R.id.progressBar && it.id != R.id.errorContainer }
+                .forEach { it.visibility = View.VISIBLE }
+            onSuccess(successData)
+        }
+    )
+}
+
+/**
+ * Assign onClick listener for default try-again button.
+ */
+fun BaseFragment.onTryAgain(root: View, onTryAgainPressed: () -> Unit) {
+    root.findViewById<Button>(R.id.tryAgainButton).setOnClickListener { onTryAgainPressed() }
+}
