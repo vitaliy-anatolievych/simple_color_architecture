1e0fd9ba51361da8baf451459970749ccde19fc8
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-23 20:15:04 +0200

    Додано альтернативні типи для LiveData з Result

--- core/src/main/java/com/study/core/views/BaseViewModel.kt
+++ core/src/main/java/com/study/core/views/BaseViewModel.kt
@@ -1,13 +1,21 @@
 package com.study.core.views
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
-
 import androidx.lifecycle.ViewModel
 import com.study.core.utils.Event
+import com.study.core.utils.Result
 
 typealias LiveEvent<T> = LiveData<Event<T>>
 typealias MutableLiveEvent<T> = MutableLiveData<Event<T>>
+
+/**
+ * Альтернативні типи для типів LiveData з Result
+ */
+typealias LiveResult<T> = LiveData<Result<T>>
+typealias MutableLiveResult<T> = MutableLiveData<Result<T>>
+typealias MediatorLiveResult<T> = MediatorLiveData<Result<T>>
 
 /**
  * Базовий клас для усіх [ViewModel]
