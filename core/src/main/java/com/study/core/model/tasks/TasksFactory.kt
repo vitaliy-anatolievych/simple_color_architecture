24c6d1d431467834ccb23abebdc62dfbd7da90d6
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-24 21:35:02 +0200

    Створено фабрику Tasks

---
+++ core/src/main/java/com/study/core/model/tasks/TasksFactory.kt
@@ -0,0 +1,15 @@
+package com.study.core.model.tasks
+
+typealias TaskBody<T> = () -> T
+
+/**
+ * Factory for creating async task instances ([Task]) from synchronous code defined by [TaskBody]
+ */
+interface TasksFactory {
+
+    /**
+     * Create a new [Task] instance from the specified body.
+     */
+    fun <T> async(body: TaskBody<T>): Tasks<T>
+
+}
