43656418e7f97591bdbfb704f3221cdbe778e364
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-24 21:59:14 +0200

    Перенесено код с CoreViewModel -> BaseViewModel

--- core/src/main/java/com/study/core/viewmodels/CoreViewModel.kt
+++ core/src/main/java/com/study/core/viewmodels/CoreViewModel.kt
@@ -3,11 +3,7 @@ package com.study.core.viewmodels
 import androidx.lifecycle.ViewModel
 import com.study.core.contracts.Navigator
 import com.study.core.contracts.UiActions
-import com.study.core.model.PendingResult
-import com.study.core.model.tasks.TaskListener
-import com.study.core.model.tasks.Tasks
 import com.study.core.navigator.NavigatorManager
-import com.study.core.views.MutableLiveResult
 
 open class CoreViewModel(
     val uiActions: UiActions,
@@ -16,36 +12,9 @@ open class CoreViewModel(
     Navigator by navigatorManager,
     UiActions by uiActions {
 
-    private val tasks = mutableSetOf<Tasks<*>>()
-
     override fun onCleared() {
         super.onCleared()
         navigatorManager.clear()
-        tasks.forEach { it.cancel() }
-        tasks.clear()
     }
 
-    /**
-     * Запускати завдання асинхронно, слухати його результат і
-     * автоматично відписує слухача у випадку знищення вьюмоделі
-     */
-    fun <T> Tasks<T>.safeEnqueue(listener: TaskListener<T>? = null) {
-        tasks.add(this)
-        this.enqueue {
-            tasks.remove(this)
-            listener?.invoke(it)
-        }
-    }
-
-    /**
-     * Асинхронний запуск задачі та зіставлення її результату з вказаним
-     * [liveResult].
-     * Задача автоматично скасовується, якщо вьюмодель буде знищено.
-     */
-    fun <T> Tasks<T>.into(liveResult: MutableLiveResult<T>) {
-        liveResult.value = PendingResult()
-        this.safeEnqueue {
-            liveResult.value = it
-        }
-    }
 }
--- core/src/main/java/com/study/core/views/BaseViewModel.kt
+++ core/src/main/java/com/study/core/views/BaseViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.study.core.model.PendingResult
 import com.study.core.utils.Event
 import com.study.core.model.Result
+import com.study.core.model.tasks.TaskListener
+import com.study.core.model.tasks.Tasks
 
 typealias LiveEvent<T> = LiveData<Event<T>>
 typealias MutableLiveEvent<T> = MutableLiveData<Event<T>>
@@ -22,11 +25,43 @@ typealias MediatorLiveResult<T> = MediatorLiveData<Result<T>>
  */
 open class BaseViewModel: ViewModel() {
 
+    private val tasks = mutableSetOf<Tasks<*>>()
+
     /**
      * Додай це метод у дочірній клас, якщо хочешь
      * слухати результат з інших єкранів
      */
     open fun onResult(result: Any) {
 
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        tasks.forEach { it.cancel() }
+        tasks.clear()
+    }
+
+    /**
+     * Запускати завдання асинхронно, слухати його результат і
+     * автоматично відписує слухача у випадку знищення вьюмоделі
+     */
+    fun <T> Tasks<T>.safeEnqueue(listener: TaskListener<T>? = null) {
+        tasks.add(this)
+        this.enqueue {
+            tasks.remove(this)
+            listener?.invoke(it)
+        }
+    }
+
+    /**
+     * Асинхронний запуск задачі та зіставлення її результату з вказаним
+     * [liveResult].
+     * Задача автоматично скасовується, якщо вьюмодель буде знищено.
+     */
+    fun <T> Tasks<T>.into(liveResult: MutableLiveResult<T>) {
+        liveResult.value = PendingResult()
+        this.safeEnqueue {
+            liveResult.value = it
+        }
     }
 }
