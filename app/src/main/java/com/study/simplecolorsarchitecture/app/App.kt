c011f6bc00e09a244526079109508c2bd1c9a85e
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-24 21:49:15 +0200

    Додано до залежностей фабрику Tasks

--- app/src/main/java/com/study/simplecolorsarchitecture/app/App.kt
+++ app/src/main/java/com/study/simplecolorsarchitecture/app/App.kt
@@ -2,11 +2,15 @@ package com.study.simplecolorsarchitecture.app
 
 import android.app.Application
 import com.study.core.contracts.ModelsProvider
+import com.study.core.model.tasks.SimpleTasksFactory
 import com.study.simplecolorsarchitecture.model.colors.InMemoryColorsRepository
 
 class App: Application(), ModelsProvider {
 
+    private val tasksFactory = SimpleTasksFactory()
+
     private val dependency = listOf<Any>(
+        tasksFactory,
         InMemoryColorsRepository()
     )
 
