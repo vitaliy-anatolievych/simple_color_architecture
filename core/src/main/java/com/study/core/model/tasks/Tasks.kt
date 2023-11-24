24b1c5fa61aaea50df409b1fb8eea2a3f599600a
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-24 21:33:34 +0200

    Створено інтерфейс для Tasks

---
+++ core/src/main/java/com/study/core/model/tasks/Tasks.kt
@@ -0,0 +1,30 @@
+package com.study.core.model.tasks
+
+import com.study.core.model.FinalResult
+
+typealias TaskListener<T> = (FinalResult<T>) -> Unit
+
+interface Tasks<T> {
+
+    /**
+     * Блокування методу очікування та отримання результатів.
+     * Згенерує виключення у випадку помилки.
+     * @throws  [IllegalStateException], якщо завдання вже виконано
+     */
+    fun await(): T
+
+    /**
+     * Неблокуючий метод для прослуховування результатів завдання.
+     * Якщо завдання скасовано до завершення, слухач не викликається.
+     *
+     * Слухач викликається в основному потоці.
+     * @throws [IllegalStateException], якщо завдання вже виконано.
+     */
+    fun enqueue(listener: TaskListener<T>)
+
+    /**
+     * Скасувати це завдання і видалити слухача, призначеного за допомогою [enqueue].
+     */
+    fun cancel()
+
+}
